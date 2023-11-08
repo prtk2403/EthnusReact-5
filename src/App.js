ec1936e4208f221f4ddb75f8d0a731fcfcf5d5f1
prtk2403 <prtk.2403@gmail.com>
2023-11-08 22:23:04 +0530

    Added Source FIles

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Required from "./components/Required";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <br />
+      <h3>React JS Task 5</h3>
+      <h3>Created by Pratheek Nistala <a href='https://github.com/prtk2403'>prtk2403</a></h3>
+      <br />
+      <br />
+      <Required></Required>
+    </div>
+  );
+}
+
+export default App;
